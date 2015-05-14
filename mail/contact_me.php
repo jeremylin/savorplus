882c609d3113fa37379b63c967a480b9f9767fc5
JeremyLin <ctjh881402@gmail.com>
2015-05-15 00:33:42 +0800

    Fix mail js php function

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -2,8 +2,8 @@
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
+   // empty($_POST['phone']) 		||
+   // empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
@@ -12,8 +12,8 @@ if(empty($_POST['name'])  		||
 	
 $name = $_POST['name'];
 $email_address = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
+// $phone = $_POST['phone'];
+// $message = $_POST['message'];
 	
 // Create the email and send the message
 $to = 'ctjh881402@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
